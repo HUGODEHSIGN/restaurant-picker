fc344fe7a478a8099354502c59c293567078a7d7
Hugo Hsi <hhsi@hugodehsign.com>
2024-04-29 01:53:33 -0400

    styled restaurant list page

--- app/routes/list.tsx
+++ app/routes/list.tsx
@@ -1,5 +1,8 @@
 import { json, useLoaderData } from '@remix-run/react';
 import { db } from 'db/drizzle';
+import { Pencil, Trash2 } from 'lucide-react';
+import { Badge } from '~/components/ui/badge';
+import { Button } from '~/components/ui/button';
 
 export async function loader() {
   const data = await db.query.restaurants.findMany({
@@ -24,20 +27,47 @@ export async function loader() {
 export default function ListPage() {
   const data = useLoaderData<typeof loader>();
   return (
-    <>
+    <div>
       {data.map(
         ({ id, name, location, mealsToRestaurants, cuisinesToRestaurants }) => (
-          <div key={id}>
-            <h1>{name}</h1>
-            <p>{location.name}</p>
-            <p>
-              {mealsToRestaurants.map((meal) => (
-                <div key={meal.mealId}>{meal.meals.name}</div>
-              ))}
-            </p>
+          <div
+            key={id}
+            className="border-t p-4 flex flex-col gap-4 hover:bg-accent">
+            <div className="flex flex-row justify-between">
+              <div>
+                <h3 className="font-bold">{name}</h3>
+                <p>{location.name}</p>
+              </div>
+              <div className="flex flex-row gap-2">
+                <Button
+                  size="icon"
+                  variant="outline">
+                  <Pencil className="h-4 w-4" />
+                </Button>
+                <Button
+                  size="icon"
+                  variant="destructive">
+                  <Trash2 className="h-4 w-4" />
+                </Button>
+              </div>
+            </div>
+            <div className="flex flex-col gap-2">
+              <div className="flex flex-row gap-2">
+                {mealsToRestaurants.map((meal) => (
+                  <Badge key={meal.mealId}>{meal.meals.name}</Badge>
+                ))}
+              </div>
+              <div className="flex flex-row gap-2">
+                {cuisinesToRestaurants.map((cuisines) => (
+                  <Badge key={cuisines.cuisineId}>
+                    {cuisines.cuisines.name}
+                  </Badge>
+                ))}
+              </div>
+            </div>
           </div>
         )
       )}
-    </>
+    </div>
   );
 }
